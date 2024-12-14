014d2b6d83e8576a0a74e14e37178f69d73859b3
Sofia Azzahra <sofiaazzahra0001@gmail.com>
2024-12-14 07:50:18 +0700

    memunculkan gambar

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -46,20 +46,18 @@ class BookController extends Controller
             'year' => 'required|integer|max:2077',
             'publisher' => 'required|max:255',
             'city' => 'required|max:50',
-            'cover' => 'required|max:255',
+            'cover' => 'nullable',
             'bookshelf_id' => 'required',
         ]);
-        if ($request->hasFile('cover')) { 
-            $path = $request->file('cover')->storeAs( 
-                'public/cover_buku',  
-                'cover_buku_'.time() . '.' . $request->file('cover')->extension() 
-            ); 
-            $validated['cover'] = basename($path); 
+        if ($request->hasFile('cover')) {
+            $path = $request->file('cover')->store('cover_buku', 'public');
+           
+            $validated['cover'] = basename($path);
         }
         Book::create($validated);
-        $notification =  array( 
-            'message' => 'Data buku berhasil ditambahkan', 
-            'alert-type' => 'success' 
+        $notification =  array(
+            'message' => 'Data buku berhasil ditambahkan',
+            'alert-type' => 'success'
         );
         if($request->save == true){
             return redirect()->route('book')->with($notification);
@@ -106,23 +104,21 @@ class BookController extends Controller
             'year' => 'required|integer|max:2077',
             'publisher' => 'required|max:255',
             'city' => 'required|max:50',
-            'cover' => 'required',
+            'cover' => 'nullable',
             'bookshelf_id' => 'required',
         ]);
-        if ($request->hasFile('cover')) { 
-            if($book->cover != null){ 
+        if ($request->hasFile('cover')) {
+            if($book->cover != null){
                 Storage::delete('public/cover_buku/'.$request->old_cover);
             }
-            $path = $request->file('cover')->storeAs( 
-                'public/cover_buku',  
-                'cover_buku_'.time() . '.' . $request->file('cover')->extension() 
-            ); 
-            $validated['cover'] = basename($path); 
+            $path = $request->file('cover')->store('cover_buku', 'public');
+           
+            $validated['cover'] = basename($path);
         }
         $book->update($validated);
-        $notification =  array( 
-            'message' => 'Data buku berhasil ditambahkan', 
-            'alert-type' => 'success' 
+        $notification =  array(
+            'message' => 'Data buku berhasil ditambahkan',
+            'alert-type' => 'success'
         );
         return redirect()->route('book')->with($notification);
     }
@@ -137,9 +133,9 @@ class BookController extends Controller
             Storage::delete('public/cover_buku/' .$book->cover);
         }
         $book->delete();
-        $notification =  array( 
-            'message' => 'Data buku berhasil dihapus', 
-            'alert-type' => 'success' 
+        $notification =  array(
+            'message' => 'Data buku berhasil dihapus',
+            'alert-type' => 'success'
         );
         return redirect()->route('book')->with($notification);
     }
